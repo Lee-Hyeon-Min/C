f60608b54adcb7a711a130c32dd0521cf92274ed
dlgusals97 <59655095+Lee-Hyeon-Min@users.noreply.github.com>
2022-05-01 23:30:32 +0900

    Create Compare.c

---
+++ Chapter 3/Compare.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    int x, y;
+   printf("input x:"); scanf_s("%d", &x);
+   printf("input y:"); scanf_s("%d", &y);
+   printf("x==y result :%d\n", x==y);
+   printf("x!=y result :%d\n", x != y);
+   printf("x>y result :%d\n", x > y);
+   printf("x<y result :%d\n",x < y);
+   printf("x>=y result :%d\n",x >= y);
+   printf("x<=y result :%d\n",x <= y);
+}
